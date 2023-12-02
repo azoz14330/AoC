21edf868c84a6b74720355e202063cdf22563136
azoz14330 <107814501+azoz14330@users.noreply.github.com>
2023-12-02 05:06:37 +0300

    Added gitignore, updated the code to something functional"
    "

--- day1/part1/script.js
+++ day1/part1/script.js
@@ -2,50 +2,80 @@ const fs = require('fs');
 const readline = require('readline');
 const filePath = "input.txt";
 finalValue = 0;
+currentline = 0;
 const lineReader = readline.createInterface({
     input: fs.createReadStream(filePath),
     crlfDelay: Infinity
   });
   lineReader.on('line', (line) => {
     // Process each line here
-    line = line.split('');
-    for(i = 0; i< line.length-1; i++){
-        numbers = [];
-        if (!line[i].isNaN==false){
-            numbers.push(parseInt(line[i]));
-        }
-        if(line[i].toLocaleLowerCase=="o"&&line[i+1].toLocaleLowerCase=="n"&&line[i+2].toLocaleLowerCase=="e"){
-            numbers.push(1);
-        }
-        if(line[i].toLocaleLowerCase=="t"&&line[i+1].toLocaleLowerCase=="w"&&line[i+2].toLocaleLowerCase=="o"){
-            numbers.push(2);
-        }
-        if(line[i].toLocaleLowerCase=="t"&&line[i+1].toLocaleLowerCase=="h"&&line[i+2].toLocaleLowerCase=="r"&&line[i+3].toLocaleLowerCase=="e"&&line[i+4].toLocaleLowerCase=="e"){
-            numbers.push(3);
-        }
-        if(line[i].toLocaleLowerCase=="f"&&line[i+1].toLocaleLowerCase=="o"&&line[i+2].toLocaleLowerCase=="u"&&line[i+3].toLocaleLowerCase=="r"){
-            numbers.push(4);
-        }
-        if(line[i].toLocaleLowerCase=="f"&&line[i+1].toLocaleLowerCase=="i"&&line[i+2].toLocaleLowerCase=="v"&&line[i+3].toLocaleLowerCase=="e"){
-            numbers.push(5);
-        }
-        if(line[i].toLocaleLowerCase=="s"&&line[i+1].toLocaleLowerCase=="i"&&line[i+2].toLocaleLowerCase=="x"){
-            numbers.push(6);
-        }
-        if(line[i].toLocaleLowerCase=="s"&&line[i+1].toLocaleLowerCase=="e"&&line[i+2].toLocaleLowerCase=="v"&&line[i+3].toLocaleLowerCase=="e"&&line[i+4].toLocaleLowerCase=="n"){
-            numbers.push(7);
-        }
-        if(line[i].toLocaleLowerCase=="e"&&line[i+1].toLocaleLowerCase=="i"&&line[i+2].toLocaleLowerCase=="g"&&line[i+3].toLocaleLowerCase=="h"&&line[i+4].toLocaleLowerCase=="t"){
-            numbers.push(8);
-        }
-        if(line[i].toLocaleLowerCase=="n"&&line[i+1].toLocaleLowerCase=="i"&&line[i+2].toLocaleLowerCase=="n"&&line[i+3].toLocaleLowerCase=="e"){
-            numbers.push(9);
-        }
-    }
-    finalValue += numbers[0]+numbers[numbers.length-1];
+    let array = [];
+    array = processString(line);
+    number1 = array[0].toString();
+    number2 = array[array.length-1].toString();
+    numberToAdd = number1[0]+number2[number2.length-1];
+    finalValue += parseInt(numberToAdd);
+    currentline++;
   });
   
   lineReader.on('close', () => {
     console.log(finalValue);
     console.log('File reading completed.');
   });
+
+  function processAnswer(array, word){
+    const wordToNumber = {
+        one: 1,
+        two: 2,
+        three: 3,
+        four: 4,
+        five: 5,
+        six: 6,
+        seven: 7,
+        eight: 8,
+        nine: 9
+      };
+
+    if(typeof(word) !== 'undefined' && word.length > 2){
+        if (wordToNumber.hasOwnProperty(word.slice(0,3))) {
+            array.push(wordToNumber[word.slice(0,3)]);
+          }
+        else if (wordToNumber.hasOwnProperty(word.slice(0,4))) {
+            array.push(wordToNumber[word.slice(0,4)]);
+          }
+        else if (wordToNumber.hasOwnProperty(word.slice(0,5))) {
+            array.push(wordToNumber[word.slice(0,5)]);
+          }
+          // else{
+            processAnswer(array, word.slice(1,word.length));
+          // }
+    }
+      return array;
+}
+function processString(word){
+    let array = [];
+    let wordArray = word.split(/(\d+)/);
+    if(wordArray.includes("")){
+        wordArray = wordArray.filter(element => {
+            return element !== "";
+        });
+    };
+    for(let i = 0; i<wordArray.length; i++){
+        if (Number.isInteger(Number(wordArray[i]))){
+            numbersArray = wordArray[i].split('');
+            for(let k = 0; k<numbersArray.length; k++){
+                array.push(parseInt(numbersArray[k]));
+            }
+        }
+        else if(wordArray[i].length>2){
+            let x = processAnswer([],wordArray[i])
+            for(let j = 0; j<x.length; j++){
+                array.push(x[j]);
+            }
+        }
+    }
+    array = array.filter(element => {
+        return element !== "";
+    });
+    return array;
+}
